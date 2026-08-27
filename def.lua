c8a11cca43fea719bc96e80901240d95b770b23d
raming69 <ramingcactus000@gmail.com>
2026-08-27 15:47:47 -0500

    Update def.lua

--- def.lua
+++ def.lua
@@ -1,82 +1,60 @@
 local env = getgenv()
 
-local aliases = {
-    ["getscriptclosure"] = {"getscriptfunction"},
-    ["hookfunction"] = {"replaceclosure"},
-    ["isexecutorclosure"] = {"checkclosure", "isourclosure"},
-    ["rconsoleclear"] = {"consoleclear"},
-    ["rconsolecreate"] = {"consolecreate"},
-    ["rconsoledestroy"] = {"consoledestroy"},
-    ["rconsoleinput"] = {"consoleinput"},
-    ["rconsoleprint"] = {"consoleprint"},
-    ["rconsolesettitle"] = {"rconsolename", "consolesettitle"},
-    ["crypt.base64encode"] = {
-        "crypt.base64.encode",
-        "crypt.base64_encode",
-        "base64.encode",
-        "base64_encode"
-    },
-    ["crypt.base64decode"] = {
-        "crypt.base64.decode",
-        "crypt.base64_decode",
-        "base64.decode",
-        "base64_decode"
-    },
-    ["isrbxactive"] = {"isgameactive"},
-    ["identifyexecutor"] = {"getexecutorname"},
-    ["queue_on_teleport"] = {"queueonteleport"},
-    ["request"] = {"http.request", "http_request"},
-    ["setclipboard"] = {"toclipboard"},
-    ["getthreadidentity"] = {
-        "getidentity",
-        "getthreadcontext"
-    },
-    ["setthreadidentity"] = {
-        "setidentity",
-        "setthreadcontext"
-    },
-    ["getscriptbytecode"] = {"dumpstring"},
-}
+if not env then warn("Krypton Error 2") return end
 
-local function setGlobal(path, value)
-    local current = env
+if getscriptclosure then env.getscriptfunction = getscriptclosure end
+if hookfunction then env.replaceclosure = hookfunction end
+if isexecutorclosure then env.isourclosure = isexecutorclosure end
+if isexecutorclosure then env.checkclosure = isexecutorclosure end
 
-    local parts = {}
-    for part in string.gmatch(path, "[^.]+") do
-        table.insert(parts, part)
-    end
-
-    for i = 1, #parts - 1 do
-        local part = parts[i]
-
-        if type(current[part]) ~= "table" then
-            current[part] = {}
-        end
-
-        current = current[part]
-    end
-
-    current[parts[#parts]] = value
+if rconsoleclear then env.consoleclear = rconsoleclear end
+if rconsolecreate then env.consolecreate = rconsolecreate end
+if rconsoledestroy then env.consoledestroy = rconsoledestroy end
+if rconsoleinput then env.consoleinput = rconsoleinput end
+if rconsoleprint then env.consoleprint = rconsoleprint end
+if rconsolesettitle then
+    env.rconsolename = rconsolesettitle
+    env.consolesettitle = rconsolesettitle
 end
 
-for functionName, functionAliases in pairs(aliases) do
-    local func = getGlobal and getGlobal(functionName) or nil
-
-    if not func then
-        local current = env
-
-        for part in string.gmatch(functionName, "[^.]+") do
-            current = current and current[part]
-        end
-
-        func = current
-    end
-
-    if type(func) == "function" then
-        for _, alias in ipairs(functionAliases) do
-            if getGlobal and getGlobal(alias) == nil then
-                setGlobal(alias, func)
-            end
-        end
-    end
+if crypt and crypt.base64encode then
+    crypt.base64 = crypt.base64 or {}
+    crypt.base64.encode = crypt.base64encode
+    env.base64 = env.base64 or {}
+    env.base64.encode = crypt.base64encode
+    env.base64_encode = crypt.base64encode
 end
+
+if crypt and crypt.base64decode then
+    crypt.base64 = crypt.base64 or {}
+    crypt.base64.decode = crypt.base64decode
+    env.base64 = env.base64 or {}
+    env.base64.decode = crypt.base64decode
+    env.base64_decode = crypt.base64decode
+end
+
+if isrbxactive then env.isgameactive = isrbxactive end
+
+if identifyexecutor then env.getexecutorname = identifyexecutor end
+
+if queue_on_teleport then env.queueonteleport = queue_on_teleport end
+
+if request then
+    env.http = env.http or {}
+    env.http.request = request
+    env.http_request = request
+end
+
+if setclipboard then env.toclipboard = setclipboard end
+
+if getthreadidentity then
+    env.getidentity = getthreadidentity
+    env.getthreadcontext = getthreadidentity
+end
+
+if setthreadidentity then
+    env.setidentity = setthreadidentity
+    env.setthreadcontext = setthreadidentity
+end
+
+if getscriptbytecode then env.dumpstring = getscriptbytecode end
